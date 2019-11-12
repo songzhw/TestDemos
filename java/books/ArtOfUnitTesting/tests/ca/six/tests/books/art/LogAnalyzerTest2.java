53b4d91ca3a9e944f487fde43d4d7e93d64fd022
songzhw <songzhw2012@gmail.com>
2019-11-12 10:48:37 -0500

    junit4 parameterized test

--- java/books/ArtOfUnitTesting/tests/ca/six/tests/books/art/LogAnalyzerTest2.java
+++ java/books/ArtOfUnitTesting/tests/ca/six/tests/books/art/LogAnalyzerTest2.java
@@ -1,8 +1,10 @@
 package ca.six.tests.books.art;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -11,12 +13,25 @@ import static org.junit.Assert.*;
 
 @RunWith(Parameterized.class)
 public class LogAnalyzerTest2 {
-    @Parameter
+    @Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
                 {"c.jpg", false},
                 {"d.slf", true}
         });
+    }
+
+    @Parameter(0)
+    public String fileName;
+    @Parameter(1)
+    public boolean result;
+
+    @Test
+    public void testIsValidLogFileName() {
+        System.out.println("members = " + fileName + " ; " + result);
+        LogAnalyzer target = new LogAnalyzer();
+        boolean isValid = target.isValidLogFileName(fileName);
+        assertEquals(result, isValid);
     }
 
 
