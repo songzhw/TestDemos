ad2a0e939017167d6846b504737d50bd0c643bac
songzhw <songzhw2012@gmail.com>
2019-11-12 10:10:40 -0500

    our first test

--- java/books/ArtOfUnitTesting/tests/ca/six/tests/books/art/LogAnalyzerTest.java
+++ java/books/ArtOfUnitTesting/tests/ca/six/tests/books/art/LogAnalyzerTest.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.*;
 public class LogAnalyzerTest {
     @Test
     public void isValidLogFileName_invalidExtension_returnsFalse() {
-
+        LogAnalyzer target = new LogAnalyzer();
+        boolean isValid = target.isValidLogFileName(".txt");
+        assertFalse(isValid);
     }
 }
