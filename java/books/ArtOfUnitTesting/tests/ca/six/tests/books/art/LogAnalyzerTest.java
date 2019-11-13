1abde9db11cec4d586b00cde9b75c37dbb7ca511
songzhw <songzhw2012@gmail.com>
2019-11-13 09:23:29 -0500

    test more details in the exception

--- java/books/ArtOfUnitTesting/tests/ca/six/tests/books/art/LogAnalyzerTest.java
+++ java/books/ArtOfUnitTesting/tests/ca/six/tests/books/art/LogAnalyzerTest.java
@@ -1,6 +1,10 @@
 package ca.six.tests.books.art;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.lang.reflect.Executable;
 
 import static org.junit.Assert.*;
 
@@ -8,9 +12,20 @@ import static org.junit.Assert.*;
    LogAnalyzerTest  is to test other scenarios without parameterized test
  */
 public class LogAnalyzerTest {
+    @Rule public ExpectedException exceptionRule = ExpectedException.none();
+
     @Test(expected = IllegalArgumentException.class)
     public void isValidLogFileName_emptyFileName_throwsException() {
         LogAnalyzer target = new LogAnalyzer();
         target.isValidLogFileName("");
+    }
+
+    @Test
+    public void isValidLogFileName_emptyFileName_throwsException2() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("fileName has to be provided. Now it's empty");
+
+        LogAnalyzer target = new LogAnalyzer();
+        target.isValidLogFileName(null);
     }
 }
