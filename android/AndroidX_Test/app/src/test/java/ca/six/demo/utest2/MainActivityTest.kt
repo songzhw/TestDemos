5521037a664787d89e52b011dbe818ffe01a989c
songzhw <songzhw2012@gmail.com>
2020-04-11 15:33:58 -0400

    error: WARN: Android SDK 29 requires Java 9 (have Java 8). Tests won't be run on SDK 29 unless explicitly requested.

--- android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/MainActivityTest.kt
+++ android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/MainActivityTest.kt
@@ -1,6 +1,20 @@
 package ca.six.demo.utest2
 
+import android.app.Application
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
 import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class MainActivityTest {
+
+    @Test
+    fun testResource() {
+        val app = getApplicationContext<Application>()
+        val appName = app.getString(R.string.app_name)
+        assertEquals("AndroidX_Test", appName)
+    }
+
 }
