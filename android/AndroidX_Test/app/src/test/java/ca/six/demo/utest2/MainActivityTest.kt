881c68dc277e714cbe82e45c1d04f84f5c34e050
songzhw <songzhw2012@gmail.com>
2020-04-11 16:35:15 -0400

    add "-noverify" and "$MODULE_DIR$". still not working

--- android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/MainActivityTest.kt
+++ android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/MainActivityTest.kt
@@ -1,12 +1,15 @@
 package ca.six.demo.utest2
 
 import android.app.Application
+import android.os.Build
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
+@Config(sdk = [Build.VERSION_CODES.LOLLIPOP])
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
