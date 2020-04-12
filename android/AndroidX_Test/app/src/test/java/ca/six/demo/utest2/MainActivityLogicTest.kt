2f858d9ee4771aaacbe070e563b91ab8dbe26a67
songzhw <songzhw2012@gmail.com>
2020-04-12 11:44:17 -0400

    test view

---
+++ android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/MainActivityLogicTest.kt
@@ -0,0 +1,40 @@
+package ca.six.demo.utest2
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import ca.six.demo.utest2.biz.main.MainActivity
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityLogicTest {
+    lateinit var scenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setup() {
+        scenario = ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @After
+    fun clean() {
+        scenario.close()
+    }
+
+    @Test
+    fun clickButton_textChangeText() {
+        onView(withId(R.id.tvMain))
+            .check(matches(withText("MainActivity")))
+
+        onView(withId(R.id.btnMain))
+            .perform(click())
+        onView(withId(R.id.tvMain))
+            .check(matches(withText("work.23")))
+    }
+}
