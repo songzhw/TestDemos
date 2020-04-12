c63ca1b7ba51c979efcb68c95879e8fffb26eaf5
songzhw <songzhw2012@gmail.com>
2020-04-12 11:10:00 -0400

    error: java.lang.RuntimeException: No activities found. Did you forget to launch the activity by calling getActivity() or startActivitySync or similar?

--- android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/MainActivityTest.kt
+++ android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/MainActivityTest.kt
@@ -2,6 +2,7 @@ package ca.six.demo.utest2
 
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
