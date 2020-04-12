e542cb0ed73e2f444775939cbc4c47f036ea5a72
songzhw <songzhw2012@gmail.com>
2020-04-12 11:36:54 -0400

    clean scenario

--- android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/MainActivityTest.kt
+++ android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/MainActivityTest.kt
@@ -1,6 +1,7 @@
 package ca.six.demo.utest2
 
 import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.typeText
@@ -9,12 +10,28 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import ca.six.demo.utest2.biz.main.MainActivity
+import org.junit.After
 import org.junit.Assert.assertNotNull
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
+    lateinit var scenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setup() {
+        scenario = launch(MainActivity::class.java)
+    }
+
+    @After
+    fun clean() {
+        // ActivityScenario does not clean up device state and may leave the activity keep running after the test finishes
+        // Calling close() in the test is highly recommended.
+        scenario.close()
+    }
+
 
     @Test
     fun testTextViewValue() {
@@ -27,7 +44,7 @@ class MainActivityTest {
     @Test
     fun recreateActivity_inputTestShouldRetainedAfterThat() {
         val name = "songzhw"
-        val scenario = launch(MainActivity::class.java)
+
 
         onView(withId(R.id.etMain))
             .perform(typeText(name))
@@ -39,6 +56,12 @@ class MainActivityTest {
             .check(matches(withText(name)))
     }
 
+    //TODO
+    @Test
+    fun recreateActivity_dataSaved() {
+        // onSaveInstanceState()
+    }
+
     @Test
     fun destroyActivity_listenerShouldBeUngreisted() {
         val scenario = launch(MainActivity::class.java)
@@ -47,7 +70,10 @@ class MainActivityTest {
 
         scenario.moveToState(Lifecycle.State.DESTROYED)
 
-        scenario.onActivity { actv -> assertNotNull(actv.observable.listener) }
+        // TODO verfiy(observable).removeListener()
+//        scenario.onActivity { actv -> assertNotNull(actv.observable.listener) }
 
     }
+
+    //TODO activity result
 }
