75f9cfb735d6f46278bf6c0e89966a0c8392ad5d
songzhw <songzhw2012@gmail.com>
2020-04-13 06:23:07 -0400

    error: java.lang.RuntimeException: Delegate runner 'org.robolectric.RobolectricTestRunner' for AndroidJUnit4 could not be loaded.

--- android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/MainActivityTest.kt
+++ android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/MainActivityTest.kt
@@ -1,6 +1,5 @@
 package ca.six.demo.utest2
 
-import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
@@ -9,10 +8,12 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import ca.six.demo.utest2.biz.main.MainActivity
 import org.junit.After
 import org.junit.Assert.assertNotNull
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -62,17 +63,13 @@ class MainActivityTest {
         // onSaveInstanceState()
     }
 
+    @Rule
+    val rule = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun destroyActivity_listenerShouldBeUngreisted() {
-        val scenario = launch(MainActivity::class.java)
-        // 初始时不为空
-        scenario.onActivity { actv -> assertNotNull(actv.observable.listener) }
-
-        scenario.moveToState(Lifecycle.State.DESTROYED)
-
-        // TODO verfiy(observable).removeListener()
-//        scenario.onActivity { actv -> assertNotNull(actv.observable.listener) }
-
+        val actv = rule.activity
+        assertNotNull(actv.observable.listener)
     }
 
     //TODO activity result
