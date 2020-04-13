9129652e08c6c43069e6441fb92c274bd56e6316
songzhw <songzhw2012@gmail.com>
2020-04-13 07:05:44 -0400

    error - as the fetchName() just return void, it's the live data get changed

--- android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/ItemsActivityTest.kt
+++ android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/ItemsActivityTest.kt
@@ -3,9 +3,13 @@ package ca.six.demo.utest2
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import ca.six.demo.utest2.biz.items.ItemsActivity
+import ca.six.demo.utest2.biz.items.ItemsViewModel
 import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
 
 @RunWith(AndroidJUnit4::class)
 class ItemsActivityTest {
@@ -22,5 +26,9 @@ class ItemsActivityTest {
         scenario.close()
     }
 
-
+    @Test
+    fun clickButton_tvShouldChange() {
+        val vm = mock(ItemsViewModel::class.java)
+        `when`(vm.fetchName()).thenReturn("2323")
+    }
 }
