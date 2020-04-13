885f371d8abaa2a4d9ab54953f2c63de2a44550d
songzhw <songzhw2012@gmail.com>
2020-04-13 06:54:57 -0400

    error: jvm target is 1.6, should be 1.8

--- android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/items/ItemsActivity.kt
+++ android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/items/ItemsActivity.kt
@@ -1,13 +1,19 @@
 package ca.six.demo.utest2.biz.items
 
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import ca.six.demo.utest2.R
 
-class ItemsActivity: AppCompatActivity(R.layout.activity_item_list) {
-
+class ItemsActivity : AppCompatActivity(R.layout.activity_item_list) {
+    val vm by viewModels<ItemsViewModel>()
 
 }
 
-class ItemsViewModel : ViewModel(){
+class ItemsViewModel(val saver: SavedStateHandle) : ViewModel() {
+    private val key_name = "my_name"
+
+    fun setName(name: String) = saver.set(key_name, name)
+    fun getName() = saver.getLiveData<String>(key_name)
 }
