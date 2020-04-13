73612494fc11e36827635207316a8bd4e223b28a
songzhw <songzhw2012@gmail.com>
2020-04-13 07:36:00 -0400

    still fail (text = ItemsActivity)

--- android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/items/ItemsActivity.kt
+++ android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/items/ItemsActivity.kt
@@ -1,21 +1,20 @@
 package ca.six.demo.utest2.biz.items
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import ca.six.demo.utest2.R
 import kotlinx.android.synthetic.main.activity_item_list.*
 import kotlin.concurrent.thread
 
 class ItemsActivity : AppCompatActivity(R.layout.activity_item_list) {
-    val vm by viewModels<ItemsViewModel>()
+    //val vm by viewModels<ItemsViewModel>()
+    lateinit var vm: ItemsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        vm = ViewModelProvider(this).get(ItemsViewModel::class.java)
 
         btnItems.setOnClickListener { vm.fetchName() }
 
@@ -27,10 +26,11 @@ class ItemsActivity : AppCompatActivity(R.layout.activity_item_list) {
 
 }
 
-open class ItemsViewModel(val saver: SavedStateHandle) : ViewModel() {
-    private val key_name = "my_name"
-    fun setName(name: String) = saver.set(key_name, name)
-    fun getName() = saver.getLiveData<String>(key_name)
+//open class ItemsViewModel(val saver: SavedStateHandle) : ViewModel() {
+open class ItemsViewModel : ViewModel() {
+//    private val key_name = "my_name"
+//    fun setName(name: String) = saver.set(key_name, name)
+//    fun getName() = saver.getLiveData<String>(key_name)
 
     open val nameLiveData = MutableLiveData<String>()
 
