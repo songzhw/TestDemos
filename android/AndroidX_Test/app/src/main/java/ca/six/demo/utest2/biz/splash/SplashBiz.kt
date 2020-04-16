a9060e2b5f56eba1136c2d2a2d5e2d487422e831
songzhw <songzhw2012@gmail.com>
2020-04-16 13:55:10 -0400

    error: ": android.os.NetworkOnMainThreadException"

--- android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/splash/SplashBiz.kt
+++ android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/splash/SplashBiz.kt
@@ -3,11 +3,12 @@ package ca.six.demo.utest2.biz.splash
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ca.six.demo.utest2.R
+import ca.six.demo.utest2.core.http.HttpEngine
 
 class SplashActivity: AppCompatActivity(R.layout.activity_splash) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
+        HttpEngine.splash()
     }
 }
