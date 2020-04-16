7793ef48d951cd0483120097cdfff5761f676ee0
songzhw <songzhw2012@gmail.com>
2020-04-16 14:43:18 -0400

    make the screen full-screen

--- android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/splash/SplashBiz.kt
+++ android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/splash/SplashBiz.kt
@@ -1,31 +1,39 @@
 package ca.six.demo.utest2.biz.splash
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import ca.six.demo.utest2.R
 import ca.six.demo.utest2.core.http.HttpEngine
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_splash.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class SplashActivity : AppCompatActivity(R.layout.activity_splash) {
+class SplashActivity : AppCompatActivity() {
     lateinit var vm: SplashViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
         vm = ViewModelProvider(this).get(SplashViewModel::class.java)
         vm.start()
+
+        vm.imageLive.observe(this,
+            Observer { imgUrl -> Picasso.get().load(imgUrl).into(ivSplashBg); })
     }
 }
 
 class SplashViewModel : ViewModel() {
+    val imageLive = MutableLiveData<String>()
+
     fun start() {
         viewModelScope.launch(Dispatchers.IO) {
             val resp = HttpEngine.splash()
-            println("szw resp = $resp")
+            imageLive.postValue(resp)
         }
     }
 }
