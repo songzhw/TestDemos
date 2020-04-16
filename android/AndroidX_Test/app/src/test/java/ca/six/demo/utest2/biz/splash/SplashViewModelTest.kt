31d9cfaf16431fce7fe2548700fa240bc3ae53f7
songzhw <songzhw2012@gmail.com>
2020-04-16 16:41:44 -0400

    still no good

--- android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/biz/splash/SplashViewModelTest.kt
+++ android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/biz/splash/SplashViewModelTest.kt
@@ -1,10 +1,15 @@
 package ca.six.demo.utest2.biz.splash
 
+import ca.six.demo.utest2.utils.CoroutineTestRule
 import kotlinx.coroutines.runBlocking
+import org.junit.Rule
 import org.junit.Test
 
 
 class SplashViewModelTest {
+    @get:Rule
+    val rule = CoroutineTestRule()
+
     @Test
     fun testStart() = runBlocking {
         val vm = SplashViewModel()
