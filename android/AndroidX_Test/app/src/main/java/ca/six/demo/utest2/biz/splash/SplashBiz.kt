939ba14ff049196e9830fd4e7c3552309b8ccc71
songzhw <songzhw2012@gmail.com>
2020-04-16 14:01:34 -0400

    要搞个scope来launch还麻烦, 所以用viewmodel就更方便了

--- android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/splash/SplashBiz.kt
+++ android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/splash/SplashBiz.kt
@@ -2,13 +2,22 @@ package ca.six.demo.utest2.biz.splash
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
 import ca.six.demo.utest2.R
 import ca.six.demo.utest2.core.http.HttpEngine
 
-class SplashActivity: AppCompatActivity(R.layout.activity_splash) {
+class SplashActivity : AppCompatActivity(R.layout.activity_splash) {
+    lateinit var vm: SplashViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         HttpEngine.splash()
+    }
+}
+
+class SplashViewModel : ViewModel() {
+    fun start(){
+
     }
 }
