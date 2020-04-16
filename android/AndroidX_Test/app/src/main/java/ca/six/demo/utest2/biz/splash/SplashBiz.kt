8405016914dad09e7e990a7afa81159e5fee7902
songzhw <songzhw2012@gmail.com>
2020-04-16 13:34:50 -0400

    add splash page

---
+++ android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/splash/SplashBiz.kt
@@ -0,0 +1,13 @@
+package ca.six.demo.utest2.biz.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ca.six.demo.utest2.R
+
+class SplashActivity: AppCompatActivity(R.layout.activity_splash) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
