73a0ea8d3bd8e403c56cf8723a56f29815facb89
songzhw <songzhw2012@gmail.com>
2020-04-16 16:26:24 -0400

    the log is: szw 11 szw 22
    it means it's starting to run coroutine, but the unit test is not wait it to finish

--- android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/biz/splash/SplashViewModelTest.kt
+++ android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/biz/splash/SplashViewModelTest.kt
@@ -1,13 +1,13 @@
 package ca.six.demo.utest2.biz.splash
 
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 
 class SplashViewModelTest {
     @Test
-    fun testStart() {
+    fun testStart() = runBlocking {
         val vm = SplashViewModel()
         vm.start()
-
     }
 }
