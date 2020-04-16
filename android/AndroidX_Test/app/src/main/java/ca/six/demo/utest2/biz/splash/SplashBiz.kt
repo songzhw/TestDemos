8d125e93d323c49c8b0f93d4402205aec031839e
songzhw <songzhw2012@gmail.com>
2020-04-16 15:03:03 -0400

    add lifecycle's event

--- android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/splash/SplashBiz.kt
+++ android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/splash/SplashBiz.kt
@@ -1,12 +1,13 @@
 package ca.six.demo.utest2.biz.splash
 
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.*
 import ca.six.demo.utest2.R
+import ca.six.demo.utest2.biz.main.MainActivity
+import ca.six.demo.utest2.core.arch.Event
 import ca.six.demo.utest2.core.http.HttpEngine
+import ca.six.demo.utest2.utils.nav
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_splash.*
 import kotlinx.coroutines.Dispatchers
@@ -24,16 +25,22 @@ class SplashActivity : AppCompatActivity() {
 
         vm.imageLive.observe(this,
             Observer { imgUrl -> Picasso.get().load(imgUrl).into(ivSplashBg); })
+
+        vm.navigationEvent.observe(this,
+            Observer { })
     }
 }
 
 class SplashViewModel : ViewModel() {
     val imageLive = MutableLiveData<String>()
+    val navigationEvent = MutableLiveData<Event<Unit>>()
 
     fun start() {
         viewModelScope.launch(Dispatchers.IO) {
             val resp = HttpEngine.splash()
             imageLive.postValue(resp)
+            Thread.sleep(2000)
+            navigationEvent.postValue(Event(Unit))
         }
     }
 }
