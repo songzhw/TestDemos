2fdf9b4d82f32d352b6f02bd4cb46cae992844c0
songzhw <songzhw2012@gmail.com>
2020-04-18 12:23:48 -0400

    fix the previous error aobut Looper.getMainLooper

--- android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/biz/splash/SplashViewModelTest.kt
+++ android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/biz/splash/SplashViewModelTest.kt
@@ -1,11 +1,15 @@
 package ca.six.demo.utest2.biz.splash
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
+import org.junit.Rule
 import org.junit.Test
 
-
+@ExperimentalCoroutinesApi
 class SplashViewModelTest {
+    @get:Rule var instantExecutorRule = InstantTaskExecutorRule()
     private val testDispatcher = TestCoroutineDispatcher()
 
     @Test
