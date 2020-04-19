8959c564a357795089fafa8ca2ed4d522b1aa9ff
songzhw <songzhw2012@gmail.com>
2020-04-19 13:03:05 -0400

    we can't return something within viewmodelScope

--- android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/home/HomeBiz.kt
+++ android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/home/HomeBiz.kt
@@ -3,13 +3,27 @@ package ca.six.demo.utest2.biz.home
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import ca.six.demo.utest2.R
+import ca.six.demo.utest2.core.http.HttpEngine
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(R.layout.activity_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 }
 
 class HomeViewModel : ViewModel() {
+
+    fun home() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val http = HttpEngine()
+            val resp = http.home()
+            return resp
+        }
+
+    }
 
 }
