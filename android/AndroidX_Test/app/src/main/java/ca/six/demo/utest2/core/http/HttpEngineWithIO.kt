be1b21c743e9878de9cfa8c6f86959d1c63e45ee
songzhw <songzhw2012@gmail.com>
2020-04-20 10:36:11 -0400

    refactor HttpEngine to remove duplicate code

--- android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/core/http/HttpEngineWithIO.kt
+++ android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/core/http/HttpEngineWithIO.kt
@@ -7,27 +7,26 @@ import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
-//TODO refctor to remove duplicate code
 open class HttpEngineWithIO {
     val http = OkHttpClient()
 
     open suspend fun home(): String = withContext(Dispatchers.IO) {
-        val resp = request("5e9c819230000056000a7ea2") //使用了OKHttp来请求
-        val data = HomeData(resp)
-        data.motto
+        val resp = request<HomeData>("5e9c819230000056000a7ea2") //使用了OKHttp来请求
+        resp.motto
     }
 
     open suspend fun items(): ItemsData = withContext(Dispatchers.IO) {
-        val resp = request("5e9891063500007100c483d2")
-        val data = ItemsData(resp)
-        data
+        request<ItemsData>("5e9891063500007100c483d2")
     }
 
-    private fun request(urlSuffix: String): String {
+    private inline fun <reified T> request(urlSuffix: String): T {
         val url = "http://www.mocky.io/v2/$urlSuffix"
         val request = Request.Builder().url(url).build()
         val call = http.newCall(request)
         val response = call.execute()
-        return response.body?.string() ?: ""
+        val responseString = response.body?.string() ?: ""
+        return T::class.java.getDeclaredConstructor(String::class.java)
+            .newInstance(responseString)
+
     }
 }
