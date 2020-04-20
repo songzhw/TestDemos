eb59add41e25147382ab656a142d8961a6c0c261
songzhw <songzhw2012@gmail.com>
2020-04-19 23:35:26 -0400

    also, we abandon `LiveData` (it's annoying to add observer for each livedata)

--- android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/home/HomeBiz.kt
+++ android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/biz/home/HomeBiz.kt
@@ -3,27 +3,33 @@ package ca.six.demo.utest2.biz.home
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import ca.six.demo.utest2.R
-import ca.six.demo.utest2.core.http.HttpEngine
-import kotlinx.coroutines.Dispatchers
+import ca.six.demo.utest2.core.http.HttpEngineWithIO
 import kotlinx.coroutines.launch
 
 class HomeActivity : AppCompatActivity(R.layout.activity_home) {
+    lateinit var vm: HomeViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        vm = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        lifecycleScope.launch {
+            val motto = vm.home()
+            println("szw motto = $motto")
+        }
     }
 }
 
 class HomeViewModel : ViewModel() {
 
-    fun home() {
-        viewModelScope.launch(Dispatchers.IO) {
-            val http = HttpEngine()
-            val resp = http.home()
-            return resp
-        }
-
+    suspend fun home(): String {
+        val http = HttpEngineWithIO()
+        val resp = http.home()
+        return resp
     }
 
 }
