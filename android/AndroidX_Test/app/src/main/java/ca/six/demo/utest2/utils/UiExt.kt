8cf5343f190d0d66263094ffb94f084240b2cfd2
songzhw <songzhw2012@gmail.com>
2020-04-21 22:13:20 -0400

    kotlin is not smart enough

--- android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/utils/UiExt.kt
+++ android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/utils/UiExt.kt
@@ -6,3 +6,13 @@ import android.content.Intent
 inline fun <reified T : Activity> Activity.nav() {
     startActivity(Intent(this, T::class.java))
 }
+
+inline fun <reified T : Activity> Activity.nav(arg: Map<String, Any>) {
+    val it = Intent(this, T::class.java)
+    for ((key, value) in arg) {
+        if (value is Int || value is String) {
+            it.putExtra(key, value)
+        }
+    }
+    startActivity(it)
+}
