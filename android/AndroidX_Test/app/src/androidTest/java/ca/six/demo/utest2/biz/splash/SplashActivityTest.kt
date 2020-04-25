c4eae064d1a76449a0a30b6654f8132bc81ae5a6
songzhw <songzhw2012@gmail.com>
2020-04-25 15:18:42 -0400

    now the navigation to home page succeed, but the test about splash failed

--- android/AndroidX_Test/app/src/androidTest/java/ca/six/demo/utest2/biz/splash/SplashActivityTest.kt
+++ android/AndroidX_Test/app/src/androidTest/java/ca/six/demo/utest2/biz/splash/SplashActivityTest.kt
@@ -1,6 +1,7 @@
 package ca.six.demo.utest2.biz.splash
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -8,7 +9,9 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import ca.six.demo.utest2.R
-import kotlinx.android.synthetic.main.activity_home.*
+import com.example.android.architecture.blueprints.todoapp.util.EspressoIdlingResource
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,6 +20,14 @@ import org.junit.runner.RunWith
 @LargeTest
 class SplashTest {
     @get:Rule val activityRule = ActivityTestRule(SplashActivity::class.java)
+
+    @Before fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+    }
+
+    @After fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+    }
 
     @Test fun testSplash_imageShouldDisplay() {
         onView(withId(R.id.ivSplashBg))
