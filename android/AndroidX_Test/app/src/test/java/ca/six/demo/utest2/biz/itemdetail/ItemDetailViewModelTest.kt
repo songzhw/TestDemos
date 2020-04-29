92e5da947ca56502f1bb03fbf77799da1cfacf74
songzhw <songzhw2012@gmail.com>
2020-04-28 20:58:02 -0400

    fix the failed test

--- android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/biz/itemdetail/ItemDetailViewModelTest.kt
+++ android/AndroidX_Test/app/src/test/java/ca/six/demo/utest2/biz/itemdetail/ItemDetailViewModelTest.kt
@@ -1,17 +1,24 @@
 package ca.six.demo.utest2.biz.itemdetail
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import ca.six.demo.utest2.utils.LiveDataTestUtil
-import kotlinx.coroutines.runBlocking
-import org.junit.Assert
+import ca.six.demo.utest2.utils.MainCoroutineRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class ItemDetailViewModelTest {
+    @get:Rule var instantExecutorRule = InstantTaskExecutorRule()
+    @get:Rule var mainCoroutineRule = MainCoroutineRule()
+
     @Test
-    fun test() = runBlocking{
+    fun test() {
         val vm = ItemDetailViewModel()
         vm.start()
 
         val event = LiveDataTestUtil.getValue(vm.textLive)
-        println(event)
+        assertEquals("hello", event)
     }
 }
