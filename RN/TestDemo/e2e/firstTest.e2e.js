41f2dbcbb243ed73efe704486b51957f5b20a901
songzhw <songzhw2012@gmail.com>
2020-11-18 20:05:15 -0500

    this works

--- RN/TestDemo/e2e/firstTest.e2e.js
+++ RN/TestDemo/e2e/firstTest.e2e.js
@@ -1,15 +1,15 @@
-describe('HomeScreen', () => {
+describe("HomeScreen", () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
 
-  it('should have home screen', async () => {
-    await expect(element(by.text('First Item'))).toBeVisible();
+  it("should have home screen", async () => {
+    await expect(element(by.text("First Item"))).toBeVisible();
   });
 
-  // it('should show hello screen after tap', async () => {
-  //   await element(by.id('hello_button')).tap();
-  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
-  // });
+  it("should show hello screen after tap", async () => {
+    await element(by.text("+")).tap();
+    await expect(element(by.text("Detail"))).toBeVisible();
+  });
 
 });
