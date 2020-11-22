701e955649bf7a8db252ae3b5240817d754cb6cb
songzhw <songzhw2012@gmail.com>
2020-11-22 11:55:09 -0500

    mock in detox is not working for now

--- RN/TestDemo/e2e/HomeScreenTest.e2e.js
+++ RN/TestDemo/e2e/HomeScreenTest.e2e.js
@@ -1,29 +1,43 @@
+import { HttpEngine } from "../src/core/HttpEngine";
+
+jest.mock("../src/core/HttpEngine");
+
 describe("HomeScreen", () => {
   beforeEach(async () => {
+    const listData = {
+      succ: true,
+      payload: [
+        {id:"001as0d", title:"Onion", category: "grocery", isDone: false, color: "#9c27b0"}
+      ]
+    }
+    HttpEngine.request.mockResolvedValue(listData)
+
     await device.reloadReactNative();
   });
 
-  test("should have home screen", async () => {
-    await expect(element(by.text("Honey"))).toBeVisible();
-  });
+  afterEach(() => HttpEngine.mockClear());
+
+  // test("should have home screen", async () => {
+  //   await expect(element(by.text("Honey"))).toBeVisible();
+  // });
 
   test("should show detail screen after tap", async () => {
     await element(by.text("Wine")).tap(); //这里会切到另一页, 但没事, Detox会等待另一页出现的
     await expect(element(by.text("#8e24aa"))).toBeVisible();
   });
 
-  test("should swipe then tap", async () => {
-    await waitFor(element(by.text("Bacon")))
-      .toBeVisible()
-      .whileElement(by.id("homeList"))
-      .scroll(250, "down");
-    await element(by.text("Bacon")).tap();
-    await expect(element(by.text("#4a148c"))).toBeVisible();
-  });
-
-  test("add button", async () => {
-    await element(by.id("btnAdd")).tap();
-    await expect(element(by.text("Add Todo Item"))).toBeVisible();
-  });
+  // test("should swipe then tap", async () => {
+  //   await waitFor(element(by.text("Bacon")))
+  //     .toBeVisible()
+  //     .whileElement(by.id("homeList"))
+  //     .scroll(250, "down");
+  //   await element(by.text("Bacon")).tap();
+  //   await expect(element(by.text("#4a148c"))).toBeVisible();
+  // });
+  //
+  // test("add button", async () => {
+  //   await element(by.id("btnAdd")).tap();
+  //   await expect(element(by.text("Add Todo Item"))).toBeVisible();
+  // });
 
 });
