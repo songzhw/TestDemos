dd74d568edeeae4d922c7fbb317fbcd8025fdc25
songzhw <songzhw2012@gmail.com>
2020-11-23 01:20:30 -0500

    add viewpager

--- RN/TestDemo/src/biz/ShowcaseScreen.tsx
+++ RN/TestDemo/src/biz/ShowcaseScreen.tsx
@@ -1,18 +1,32 @@
 import React from "react";
 import { View, ViewProps, Text, StyleSheet } from "react-native";
+import ViewPager from "@react-native-community/viewpager";
 
-interface IProps extends ViewProps  {}
+interface IProps extends ViewProps {
+}
 
 export const ShowcaseScreen = (props: IProps) => {
 
   return (
-    <View>
-
+    <View style={styles.root}>
+      <ViewPager style={styles.viewpager}>
+        <View key={"1"}>
+          <Text style={{ backgroundColor: "red", flex: 1 }}> First Page</Text>
+        </View>
+        <View key={"2"}>
+          <Text style={{ backgroundColor: "blue", flex: 1 }}> Second Page</Text>
+        </View>
+        <View key={"3"}>
+          <Text style={{ backgroundColor: "green", flex: 1 }}> Third Page</Text>
+        </View>
+      </ViewPager>
     </View>
-  )
-}
+  );
+};
 
 
 const styles = StyleSheet.create({
-  root: {}
+  root: { flex: 1 },
+  viewpager: { height: 200 },
+  text: {}
 });
