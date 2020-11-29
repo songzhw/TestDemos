fe077338aad4f0e28b6be1a7e2a4ec6116a3bad8
songzhw <songzhw2012@gmail.com>
2020-11-29 00:20:01 -0500

    test passes

--- RN/TestDemo/src/biz/webview/LocalHtml.ts
+++ RN/TestDemo/src/biz/webview/LocalHtml.ts
@@ -4,12 +4,15 @@ export const html = `
 <head>
   <script>
     function resetText(){
+      const str = "(default)";
       let h1 = document.getElementById("title");
-      h1.innerHTML = "(default)";
+      h1.innerHTML = str;
+      window.ReactNativeWebView.postMessage(str);
     }
     function changeText(text) {
       let h1 = document.getElementById("title");
       h1.innerHTML = text;
+      window.ReactNativeWebView.postMessage(text);
     }
   </script>
 </head>
--- RN/TestDemo/src/biz/webview/SomeWebContentScreen.tsx
+++ RN/TestDemo/src/biz/webview/SomeWebContentScreen.tsx
@@ -3,6 +3,7 @@ import { View, ViewProps, Text, StyleSheet, SafeAreaView, TextInput, TouchableOp
 import WebView from "react-native-webview";
 import { html } from "./LocalHtml";
 import { Button } from "../../ui/Button";
+import { WebViewMessageEvent } from "react-native-webview/lib/WebViewTypes";
 
 
 interface IProps extends ViewProps {
@@ -21,8 +22,14 @@ export const SomeWebContentScreen = (props: IProps) => {
   };
 
   const [script, setScript] = useState("");
+  const [callbackText, setCallbackText] = useState("")
   const onButtonPress = () => {
     webviewRef.current?.injectJavaScript(script);
+  };
+
+  const onMessage = (event: WebViewMessageEvent) => {
+    const str = event.nativeEvent.data;
+    setCallbackText(str);
   };
 
   return (
@@ -30,11 +37,11 @@ export const SomeWebContentScreen = (props: IProps) => {
       <Button style={styles.button} onClick={inject1} text={"inject1"} textStyle={{ fontSize: 20 }} />
       <Button style={styles.button} onClick={inject2} text={"inject2"} textStyle={{ fontSize: 20 }} />
 
-      <Text testID="inviLabel" style={styles.invisible}></Text>
+      <Text testID="inviLabel" style={styles.invisible}>{callbackText}</Text>
       <TextInput testID="inviInput" style={styles.invisible} onChangeText={setScript} value={script} />
       <TouchableOpacity testID="inviButton" style={styles.invisible} onPress={onButtonPress} />
 
-      <WebView ref={webviewRef} source={{ html: html }} />
+      <WebView ref={webviewRef} source={{ html: html }} onMessage={onMessage} />
     </SafeAreaView>
   );
 };
