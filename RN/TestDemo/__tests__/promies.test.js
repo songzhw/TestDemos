6d1c00fd7059298649810bef22f314219a110bd6
songzhw <songzhw2012@gmail.com>
2021-05-09 16:10:55 -0400

    test promise

--- RN/TestDemo/__tests__/promies.test.js
+++ RN/TestDemo/__tests__/promies.test.js
@@ -1,6 +1,21 @@
 import { tryDoIt } from "../src/unitest/promises";
 
-test("promise", () => {
-  tryDoIt(2)
-    .then(num => expect(num).toBe(5));
+describe("promise test", () => {
+
+  test("promise 1", (done) => {
+    tryDoIt(2)
+      .then(num => {
+        expect(num).toBe(4);
+        done();
+      });
+  });
+
+  test("promise2", () => {
+    return expect(tryDoIt(4)).resolves.toBe(8);
+  });
+
+  test("promise3", () => {
+    return expect(tryDoIt(3)).rejects.toMatch("only even");
+  });
+
 });
