25cb2c127c0829bc86188460d545f5b180b30738
songzhw <songzhw2012@gmail.com>
2021-05-31 08:20:37 -0400

    debug does not print the tree I expected

--- react/react-test/src/__tests__/page/order/SundaesOptionsView.test.js
+++ react/react-test/src/__tests__/page/order/SundaesOptionsView.test.js
@@ -1,3 +1,9 @@
-describe("SundaesOptionsView", () => {
+import { SundaesOptionsView } from "../../../pages/order/SundaesOptionsView";
+import { render } from "@testing-library/react";
 
+describe("SundaesOptionsView", () => {
+  test("", () => {
+    const { debug } = render(<SundaesOptionsView type={"scoops"} />);
+    debug();
+  });
 });
