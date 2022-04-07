3336adfde706a77846580a98e4453be2277f4b8d
songzhw <songzhw2012@gmail.com>
2022-04-07 09:50:38 -0400

    single task for two DLActv

--- android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/core/router/DeepLinkActivity.kt
+++ android/AndroidX_Test/app/src/main/java/ca/six/demo/utest2/core/router/DeepLinkActivity.kt
@@ -1,14 +1,28 @@
 package ca.six.demo.utest2.core.router
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ca.six.demo.utest2.R
+import kotlinx.android.synthetic.main.activity_main.*
 
-class DeepLinkActivity: AppCompatActivity() {
+class DeepLinkActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val uri = intent.data
-        println("szw got : $uri")
+        onReceiveDeepLink()
     }
+
+    // DeepLinkActivity.launchMode是SingleTask, 即自带了singleTop属性
+    // 栈顶为DeepLinkActivity, 要再走DeepLink, 那不应该有2个DeepLinkActivity的!
+    // 备注: LaunchMode是不需要affinity也能工作的, 所以DeepLinkActivity没有affnity也能调用到这
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        onReceiveDeepLink()
+    }
+
+    private fun onReceiveDeepLink() {
+        val uri = intent.data
+        tvMain.text = uri.toString()
+    }
+
 }
