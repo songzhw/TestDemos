93cf4e21b3a74e78cae58acfe4f51448b5b30602
songzhw <songzhw2012@gmail.com>
2025-10-06 11:20:09 -0400

    运行失败: Exception in thread "main" org.openqa.selenium.SessionNotCreatedException: Could not start a new session. Possible causes are invalid address of the remote server or browser start-up failure.       Host info: host: 'Song-Work-Mac.local', ip: '2607:fea8:f03:7c00:0:0:0:63a4%en0'

--- android/Appium01/Appium01/src/main/kotlin/Main.kt
+++ android/Appium01/Appium01/src/main/kotlin/Main.kt
@@ -1,7 +1,16 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import io.appium.java_client.AppiumBy
+import io.appium.java_client.android.AndroidDriver
+import io.appium.java_client.android.options.UiAutomator2Options
+import java.net.URI
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val options = UiAutomator2Options()
+        .setUdid("123456") //UDID: 设备级别的唯一标识符
+        .setApp("~/Downloads/apk/one.apk")
+    val driver = AndroidDriver(URI("http://127.0.0.1:4723").toURL(), options)
+
+    val el = driver.findElement(AppiumBy.xpath("//TextView"))
+    println("tv = ${el.text}")
+    println("pageSource = ${driver.pageSource}")
+    driver.quit()
 }
