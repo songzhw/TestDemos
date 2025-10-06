cfaa4aa1dccf06c49336a4d7542b3625f1be1487
songzhw <songzhw2012@gmail.com>
2025-10-06 11:30:00 -0400

    运行起来了app, 但findElement失败

--- android/Appium01/Appium01/src/main/kotlin/cn/appium/t01/Main.kt
+++ android/Appium01/Appium01/src/main/kotlin/cn/appium/t01/Main.kt
@@ -1,3 +1,5 @@
+package cn.appium.t01
+
 import io.appium.java_client.AppiumBy
 import io.appium.java_client.android.AndroidDriver
 import io.appium.java_client.android.options.UiAutomator2Options
@@ -6,7 +8,7 @@ import java.net.URI
 fun main(args: Array<String>) {
     val options = UiAutomator2Options()
         .setUdid("emulator-5554") // UDID: 设备级别的唯一标识符;  这个id由`adb devices`中可得知设备id是什么, 却要运行测试到这个设备上
-        .setApp("/Downloads/apk/one.apk")
+        .setApp("Downloads/apk/one.apk")
     val driver = AndroidDriver(URI("http://127.0.0.1:4723").toURL(), options)
 
     val el = driver.findElement(AppiumBy.xpath("//TextView"))
