5af0610cdce43c6cc5a3853ad905d309ac1ac0f4
songzhw <songzhw2012@gmail.com>
2025-10-06 11:25:49 -0400

    运行了appium的server后仍不行,Exception in thread "main" org.openqa.selenium.SessionNotCreatedException: Could not start a new session. Response code 500. Message: The application at '/Downloads/apk/one.apk' does not exist or is not accessible

--- android/Appium01/Appium01/src/main/kotlin/Main.kt
+++ android/Appium01/Appium01/src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ import java.net.URI
 
 fun main(args: Array<String>) {
     val options = UiAutomator2Options()
-        .setUdid("123456") //UDID: 设备级别的唯一标识符
+        .setUdid("emulator-5554") // UDID: 设备级别的唯一标识符;  这个id由`adb devices`中可得知设备id是什么, 却要运行测试到这个设备上
         .setApp("/Downloads/apk/one.apk")
     val driver = AndroidDriver(URI("http://127.0.0.1:4723").toURL(), options)
 
