01dadac36cb99d2a0838acc4586ecb0cd52efff4
songzhw <songzhw2012@gmail.com>
2025-10-06 11:24:13 -0400

    运行了appium的server后仍不行, Exception in thread "main" org.openqa.selenium.SessionNotCreatedException: Could not start a new session. Response code 500. Message: Device 123456 was not in the list of connected devices

--- android/Appium01/Appium01/src/main/kotlin/Main.kt
+++ android/Appium01/Appium01/src/main/kotlin/Main.kt
@@ -6,7 +6,7 @@ import java.net.URI
 fun main(args: Array<String>) {
     val options = UiAutomator2Options()
         .setUdid("123456") //UDID: 设备级别的唯一标识符
-        .setApp("~/Downloads/apk/one.apk")
+        .setApp("/Downloads/apk/one.apk")
     val driver = AndroidDriver(URI("http://127.0.0.1:4723").toURL(), options)
 
     val el = driver.findElement(AppiumBy.xpath("//TextView"))
