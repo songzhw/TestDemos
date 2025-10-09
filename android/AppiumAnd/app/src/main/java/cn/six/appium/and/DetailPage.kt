dbfcd2e00ff951a6758c7b905189838797676cd6
songzhw <songzhw2012@gmail.com>
2025-10-09 10:48:56 -0400

    detail page

--- android/AppiumAnd/app/src/main/java/cn/six/appium/and/DetailPage.kt
+++ android/AppiumAnd/app/src/main/java/cn/six/appium/and/DetailPage.kt
@@ -1,7 +1,30 @@
 package cn.six.appium.and
 
+import android.os.Bundle
+import cn.six.appium.and.biz.Cart
 import cn.six.appium.and.core.BasePage
+import cn.six.appium.and.databinding.AfDetailsBinding
 
 // img, tv, edittext,
 class DetailPage: BasePage() {
+    private lateinit var vb: AfDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        vb = AfDetailsBinding.inflate(layoutInflater)
+        setContentView(vb.root)
+
+        val title = intent.getStringExtra("title") ?: return
+        val imageResId = intent.getIntExtra("img", 0)
+        vb.tvName.text = title
+        vb.ivProduct.setImageResource(imageResId)
+        vb.etCount.setText(Cart.map.getOrDefault(title, 0).toString())
+        vb.fabSave.setOnClickListener {
+            val count = vb.etCount.text.toString().toInt()
+            if(count > 0) {
+                Cart.map[title] = count
+            }
+        }
+
+    }
 }
