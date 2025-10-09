3b252ea79adef6ef9e1a9dfee745fd8653a5d9ff
songzhw <songzhw2012@gmail.com>
2025-10-09 11:01:48 -0400

    多结果时会找到第一个为结果而返回

--- android/Appium01/Appium01/src/main/kotlin/cn/appium/t01/helloworld/UiAutomator2_HelloWorld.kt
+++ android/Appium01/Appium01/src/main/kotlin/cn/appium/t01/helloworld/UiAutomator2_HelloWorld.kt
@@ -5,6 +5,7 @@ import io.appium.java_client.android.AndroidDriver
 import io.appium.java_client.android.options.UiAutomator2Options
 import java.net.URI
 
+// 使用AppiumBy.className("android.widget.TextView")时, 当有多个TextView时, 会找到第一个textView哦
 fun main() {
     val options = UiAutomator2Options()
         .setUdid("emulator-5554") // UDID: 设备级别的唯一标识符;  这个id由`adb devices`中可得知设备id是什么, 却要运行测试到这个设备上
