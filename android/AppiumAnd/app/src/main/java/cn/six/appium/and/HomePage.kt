8c7df76736ab69d969d77864c8a73889664a8874
songzhw <songzhw2012@gmail.com>
2025-10-11 00:42:23 -0400

    现在完美了, 刷新 + 保存上次滑动位置全成功!!!

--- android/AppiumAnd/app/src/main/java/cn/six/appium/and/HomePage.kt
+++ android/AppiumAnd/app/src/main/java/cn/six/appium/and/HomePage.kt
@@ -7,7 +7,6 @@ import androidx.core.os.bundleOf
 import androidx.core.view.updateLayoutParams
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import cn.six.appium.and.biz.Cart
 import cn.six.appium.and.core.BasePage
 import cn.six.appium.and.databinding.AfHomeBinding
@@ -19,30 +18,50 @@ import cn.six.appium.and.views.rv.adapters.builder.BuilderItem
 import cn.six.appium.and.views.rv.adapters.builder_diff.DiffBuilderCallback
 import cn.six.appium.and.views.rv.adapters.builder_diff.MatchEachDiffResult
 
-// 有多个页, 有img, 有button, 有editText, 有drawer,
-// 有recyclerView (查找某item; 要滑动才能找到某item),
+// 1. 若adapter不是成员, 那就会有问题:
+//  : 刷新是有刷新  (DiffUtil + ListAdapter), 但刷新后总是回到rv第0项的位置
+//    原因是rv每次都用新的adapter, 那自然就会认为整个rv都刷新了, 所以刷新后就重新到了第0项
+//    解决之法就是用一个adapter成员, 这样之后的adapter.submitList()就不会刷新rv
 class HomePage : BasePage() {
     private lateinit var vb: AfHomeBinding
+    private lateinit var adapter: ListAdapter<BuilderItem, RvViewHolder>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         vb = AfHomeBinding.inflate(layoutInflater)
         setContentView(vb.root)
+
+        val context = vb.rvHome.context
+        vb.rvHome.layoutManager = LinearLayoutManager(context)
+        val wrapper = assembleBuildItemList()
+        val diff = DiffBuilderCallback(
+            arrayListOf(
+                ShowcaseItem.Companion::isSame,
+                TitleBuilderItem.Companion::isSame,
+                DividerBuilderItem.Companion::isSame
+            )
+        )
+        adapter = wrapper.generateDiffAdapter(diff)
+        vb.rvHome.adapter = adapter
+        adapter.submitList(wrapper.list)
+
     }
 
     override fun onResume() {
         super.onResume()
-        println("szww refresh HomePage")
         // 用DiffUtil当然性能列好, 但这里为了简便就先不用了
-        commonAndTitleVerticalLayout(vb.rvHome)
+        refresh(adapter)
     }
 
 }
 
 
-fun commonAndTitleVerticalLayout(rv: RecyclerView) {
-    val context = rv.context
-    rv.layoutManager = LinearLayoutManager(context)
+fun refresh(adapter: ListAdapter<BuilderItem, RvViewHolder>) {
+    val wrapper = assembleBuildItemList()
+    adapter.submitList(wrapper.list)
+}
+
+private fun assembleBuildItemList(): BuilderAdapterWrapper {
     val wrapper = BuilderAdapterWrapper()
     wrapper.add(TitleBuilderItem(TitleDataForGroup(R.mipmap.ic_launcher, "风景")))
     wrapper.add(ShowcaseItem("apple", R.drawable.ic_apple))
@@ -55,12 +74,7 @@ fun commonAndTitleVerticalLayout(rv: RecyclerView) {
     wrapper.add(ShowcaseItem("fig", R.drawable.ic_fig))
     wrapper.add(ShowcaseItem("grape", R.drawable.ic_grape))
     wrapper.add(DividerBuilderItem(100))
-    val diff = DiffBuilderCallback(arrayListOf(
-        ShowcaseItem.Companion::isSame, TitleBuilderItem.Companion::isSame, DividerBuilderItem.Companion::isSame
-    ))
-    val adapter = wrapper.generateDiffAdapter(diff)
-    rv.adapter = adapter
-    adapter.submitList(wrapper.list)
+    return wrapper
 }
 
 class ShowcaseItem(val title: String, val imageResId: Int) : BuilderItem {
@@ -87,7 +101,7 @@ class ShowcaseItem(val title: String, val imageResId: Int) : BuilderItem {
 
     private fun updateCount(vh: RvViewHolder) {
         val count = Cart.map.getOrDefault(title, 0)
-        val visible = if(count <= 0) View.GONE else View.VISIBLE
+        val visible = if (count <= 0) View.GONE else View.VISIBLE
         vh.setVisibility(R.id.tvCount, visible)
         vh.setText(R.id.tvCount, count.toString())
     }
@@ -132,6 +146,7 @@ data class DividerBuilderItem(val heightInDp: Int) : BuilderItem {
             height = heightInDp.dpToPx()
         }
     }
+
     companion object {
         fun isSame(old: BuilderItem, now: BuilderItem): MatchEachDiffResult {
             if (old is DividerBuilderItem && now is DividerBuilderItem) {
